61f02f2578d6925226e1498308d3488aad2b4ebc
Bachptit2003 <bach06092003@gmail.com>
2025-11-22 19:08:11 +0700

    first commit

---
+++ 02_Library_Management_System/src/book.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "book.h"
+
+void bookMenu() {
+    printf("Book management module\n");
+}
---
+++ 02_Library_Management_System/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "book.h"
+#include "borrow.h"
+#include "user.h"
+
+int main()
+{
+    printf("Library management system\n");
+    return 0;
+}
